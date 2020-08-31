e1cf59d5ef6d652400f43dca6015624a43e78351
abc <nikhil.singh3_cs19@gla.ac.in>
2020-08-30 20:26:02 -0700

    created getter setter constructor and toString method for class Book

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -7,4 +7,36 @@
 package definitions;
 
 public class Book {
+    private String nameOfBook;
+    private String nameOfAuthorOfBook;
+    private String isbnNumberOfBook;
+
+    public String getNameOfBook(){
+        return nameOfBook;
+    }
+    public String getNameOfAuthorOfBook(){
+        return nameOfAuthorOfBook;
+    }
+    public String getIsbnNumberOfBook(){
+        return isbnNumberOfBook;
+    }
+
+    public void setNameOfBook(String nameOfBook){
+        this.nameOfBook=nameOfBook;
+    }
+    public void setNameOfAuthorOfBook(String nameOfAuthorOfBook){
+        this.nameOfAuthorOfBook=nameOfAuthorOfBook;
+    }
+    public void setIsbnNumberOfBook(String isbnNumberOfBook){
+        this.isbnNumberOfBook=isbnNumberOfBook;
+    }
+
+    public Book(String nameOfBook,String nameOfAuthorOfBook,String isbnNumberOfBook){
+        this.nameOfBook=nameOfBook;
+        this.nameOfAuthorOfBook=nameOfAuthorOfBook;
+        this.isbnNumberOfBook=isbnNumberOfBook;
+    }
+    public String toString(){
+        return nameOfBook+","+nameOfAuthorOfBook+","+isbnNumberOfBook;
+    }
 }

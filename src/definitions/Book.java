69f051e3e63f6e90d08af9492f4c74851dfb7484
abc <nikhil.singh3_cs19@gla.ac.in>
2020-08-30 20:37:10 -0700

    created equals and hashCode method for Book class

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -6,6 +6,8 @@
  * */
 package definitions;
 
+import java.util.Objects;
+
 public class Book {
     private String nameOfBook;
     private String nameOfAuthorOfBook;
@@ -36,7 +38,23 @@ public class Book {
         this.nameOfAuthorOfBook=nameOfAuthorOfBook;
         this.isbnNumberOfBook=isbnNumberOfBook;
     }
+    @Override
     public String toString(){
         return nameOfBook+","+nameOfAuthorOfBook+","+isbnNumberOfBook;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Book)) return false;
+        Book book = (Book) o;
+        return Objects.equals(getNameOfBook(), book.getNameOfBook()) &&
+                Objects.equals(getNameOfAuthorOfBook(), book.getNameOfAuthorOfBook()) &&
+                Objects.equals(getIsbnNumberOfBook(), book.getIsbnNumberOfBook());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getNameOfBook(), getNameOfAuthorOfBook(), getIsbnNumberOfBook());
     }
 }
